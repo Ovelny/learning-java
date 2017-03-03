59b1e402c46cda294dba798ce30f7f9c2f2e3167
Ovelny <this.is.ovelny@gmail.com>
2017-03-03 14:31:37 +0100

    loadUsers and checkLogin

--- src/cesi/ril41/Program.java
+++ src/cesi/ril41/Program.java
@@ -15,10 +15,11 @@ import cesi.ril41.exceptions.PasswordException;
 
 public class Program {
 
+	public static List<CompteUtilisateur> comptesUtilisateur = new ArrayList<CompteUtilisateur>();
+
 	public static void main(String[] args) {
 
 		// Initisialisation des 5 utilisateurs
-		List<CompteUtilisateur> comptesUtilisateur = new ArrayList<CompteUtilisateur>();
 		/*
 		 * comptesUtilisateur.add(new CompteUtilisateur(1, "lanrent", "remy",
 		 * new generateurMDPSimple())); comptesUtilisateur.add(new
@@ -30,6 +31,21 @@ public class Program {
 		 * CompteAdministrateur(5, "john", "doe", new generateurMDPComplex()));
 		 */
 
+		Window panel = new Window();
+		panel.setVisible(true);
+
+		// input console
+
+		// System.out.println("Veuillez saisir votre identifiant");
+		//
+		// Scanner inputUser = new Scanner(System.in);
+		// String inputIdentifiant = inputUser.nextLine();
+		// System.out.println("Veuillez saisir votre mot de passe");
+		// String inputPassword = inputUser.nextLine();
+		// inputUser.close();
+	}
+
+	public static void loadUsers() {
 		Scanner monLecteur;
 		try {
 			monLecteur = new Scanner(new File("utilisateurs.txt"));
@@ -41,63 +57,48 @@ public class Program {
 
 			}
 			monLecteur.close();
-		} 
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		Window panel = new Window();
-	    panel.setVisible(true);
-	    
-//		System.out.println(comptesUtilisateur.get(1).getNom());
-//		// input console
-//
-//		System.out.println("Veuillez saisir votre identifiant");
-//
-//		Scanner inputUser = new Scanner(System.in);
-//		String inputIdentifiant = inputUser.nextLine();
-//		System.out.println("Veuillez saisir votre mot de passe");
-//		String inputPassword = inputUser.nextLine();
-//		inputUser.close();
-//
-//		Identifiant identifiant = new Identifiant(inputIdentifiant, inputPassword);
-//		boolean goodLogin = false;
-//		boolean goodPassword = false;
-//		
-//		// Caclule le poids de chaque utilisateur
-//		for (int i = 0; i < comptesUtilisateur.size(); i++) {
-//			Identifiant user = comptesUtilisateur.get(i).genererIdentifiant();
-//			// System.out.println("Mot de passe généré : " +
-//			// user.getMotDePasse());
-//			// System.out.println("Somme de controle trouvé : " +
-//			// user.getSommeDeControle());
-//
-//			if (identifiant.testSommeControle(user.getSommeDeControle())) {
-//				System.out.println("Bienvenue !");
-//				goodLogin = true;
-//				goodPassword = true;
-//				break;
-//			}
-//			
-//			if (identifiant.getLogin().equals(user.getLogin())) {
-//				goodLogin = true;
-//				break;
-//			}
-//
-//		}
-//		
-//		try {
-//			
-//			if (!goodLogin) {
-//				throw new IdentifiantException("Identifiant inconnu.");
-//			}
-//
-//			if (goodLogin && !goodPassword) {
-//				throw new PasswordException("Mot de passe erroné.");
-//			}
-//		} 
-//		catch (Exception e) {
-//			System.out.println(e.getMessage());
-//		}
+	}
+
+	public static void checkLogin(Identifiant identifiant) {
+		boolean goodLogin = false;
+		boolean goodPassword = false;
+
+		// Caclule le poids de chaque utilisateur
+		for (int i = 0; i < comptesUtilisateur.size(); i++) {
+			Identifiant user = comptesUtilisateur.get(i).genererIdentifiant();
+			// System.out.println("Mot de passe généré : " +
+			// user.getMotDePasse());
+			// System.out.println("Somme de controle trouvé : " +
+			// user.getSommeDeControle());
+
+			if (identifiant.testSommeControle(user.getSommeDeControle())) {
+				System.out.println("Bienvenue !");
+				goodLogin = true;
+				goodPassword = true;
+				break;
+			}
+
+			if (identifiant.getLogin().equals(user.getLogin())) {
+				goodLogin = true;
+				break;
+			}
+
+		}
+
+		try {
+
+			if (!goodLogin) {
+				throw new IdentifiantException("Identifiant inconnu.");
+			}
+
+			if (goodLogin && !goodPassword) {
+				throw new PasswordException("Mot de passe erroné.");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
--- src/cesi/ril41/Window.java
+++ src/cesi/ril41/Window.java
@@ -1,7 +1,12 @@
 package cesi.ril41;
 import javax.swing.*;
+
+import cesi.ril41.autentification.Identifiant;
+
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.GridBagConstraints;
 
 public class Window  extends JFrame {
@@ -13,16 +18,19 @@ public class Window  extends JFrame {
 	private JPasswordField passwordField = new JPasswordField(20);
 	
 	public Window() {
+		Program.loadUsers();
 		JPanel loginPanel  = new JPanel(new GridBagLayout());
 		GridBagConstraints constraints = new GridBagConstraints();
 	    constraints.anchor = GridBagConstraints.WEST;
 	    constraints.insets = new Insets(10, 10, 10, 10);
 	    
+	    // settings de base
 		this.setContentPane(loginPanel);
 		this.setSize(400,300);
 	    this.setResizable(false);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    
+	    // placements des boutons et champs
 	    constraints.gridx = 0;
         constraints.gridy = 0;     
         this.add(loginTxt, constraints);
@@ -49,11 +57,40 @@ public class Window  extends JFrame {
         constraints.anchor = GridBagConstraints.CENTER;
         this.add(reinitBtn, constraints);
         
+        // action listeners et action performed
+        okBtn.addActionListener(new ActionListener() {
+        	
+			public void actionPerformed(ActionEvent e) {
+        		String inputIdentifiant = loginField.getText();
+        		String inputPassword = new String(passwordField.getPassword());
+        		
+        		Identifiant identifiant = new Identifiant(inputIdentifiant, inputPassword);
+        		Program.checkLogin(identifiant);
+        	}
+        });
+        
+        reinitBtn.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		reinitPerformed();
+        	}
+        });
+        
+        
+        
 //		this.add(okBtn);
 //		this.add(reinitBtn);
 //		this.add(loginField);
 //		this.add(passwordField);
 //		this.add(loginTxt);
 //		this.add(passwordTxt);
+	}
+	
+	public void okPerformed() {
+		System.out.println("ok working");
+	}
+	
+	public void reinitPerformed() {
+		loginField.setText("");
+		passwordField.setText("");
 	}
 }
